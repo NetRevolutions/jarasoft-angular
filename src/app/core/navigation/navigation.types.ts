12e35989cdbb898b34c2559acfd54493de03c818
NetRevolutions <j_rodriguez_arruela@hotmail.com>
2023-09-24 01:50:25 -0500

    implement navigation in core folder

---
+++ src/app/core/navigation/navigation.types.ts
@@ -0,0 +1,7 @@
+import { NavigationItem } from "./navigation-item";
+
+export interface Navigation 
+{
+    default: NavigationItem[];
+    //TODO: In the future implement other types of navigation
+}
