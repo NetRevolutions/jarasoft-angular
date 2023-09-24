8648fffd9ce9d37e9bf70bc3af2bf8758bacb3d2
NetRevolutions <j_rodriguez_arruela@hotmail.com>
2023-09-24 14:15:53 -0500

    fix comming-soon name by coming-soon

--- src/app/modules/admin/pages/coming-soon/coming-soon.component.ts
+++ src/app/modules/admin/pages/coming-soon/coming-soon.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-comming-soon',
+  selector: 'coming-soon',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './comming-soon.component.html',
+  templateUrl: './coming-soon.component.html',
   styles: [
   ]
 })
-export class CommingSoonComponent {
+export class ComingSoonComponent {
 
 }
