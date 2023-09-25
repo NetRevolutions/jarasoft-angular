1bb6d88cd4d7df5e9d20b8d057f06a2c1aaf7374
NetRevolutions <j_rodriguez_arruela@hotmail.com>
2023-09-25 10:52:45 -0500

    Add typography files

---
+++ src/app/modules/admin/ui/typography/typography.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-typography',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './typography.component.html',
+  styles: [
+  ]
+})
+export class TypographyComponent {
+
+}
